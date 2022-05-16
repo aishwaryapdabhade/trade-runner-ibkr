8e99ff72746aaf3e8d1aa9cc0340e15f9b07826a
Aishwarya Dabhade <aishwarya.dabhade94@gmail.com>
2022-05-16 18:32:24 -0400

    initial commit

---
+++ src/main/java/io/github/ftltraveller/traderunneribkr/TraderunneribkrApplication.java
@@ -0,0 +1,13 @@
+package io.github.ftltraveller.traderunneribkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderunneribkrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderunneribkrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ftltraveller/traderunneribkr/TraderunneribkrApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ftltraveller.traderunneribkr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraderunneribkrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
